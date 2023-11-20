a39002669c01915f077d20f6d4dff1c78d9f6c2d
Sentuar <thayward2001@gmail.com>
2023-11-21 11:43:54 +1300

    chore: tried to remove apikey from commit history

--- server/routes/galleryRoutes.ts
+++ server/routes/galleryRoutes.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 import { createClient } from 'pexels'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const router = express.Router()
-const pixels = createClient(
-  'u5m02W1DcYNxRW5GBU0RA320LnW2JhxD3UXRS0MqnuC36twM8Plwsdfp'
-)
+const pixels = createClient(process.env.PEXELS_API_KEY || '')
 
 const getImages = async () => {
   const query = 'Nature'
-  const images = await pixels.photos.search({ query, per_page: 6 })
+  const images = await pixels.photos.search({ query, per_page: 1 })
   return images
 }
 
