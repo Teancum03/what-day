699209cb8b997a1ba154ee42df72d0d4cb992be7
Courtney Anderson <142571170+courtney-anderson@users.noreply.github.com>
2023-11-22 11:01:29 +1300

    feat: add addWin db function

--- server/db/functions/winsDB.ts
+++ server/db/functions/winsDB.ts
@@ -2,7 +2,14 @@ import connection from '../connection'
 
 import { Wins } from '@models/wins'
 
-export async function getAllWins(db = connection): Promise<Wins[]> {
+const db = connection
+
+export async function getAllWins(): Promise<Wins[]> {
   const wins = await db<Wins>('wins').select('title', 'author')
   return wins
+}
+
+export async function addWin(newWin: Wins): Promise<Wins> {
+  const [win] = await db('win').insert(newWin).returning('*')
+  return win
 }
