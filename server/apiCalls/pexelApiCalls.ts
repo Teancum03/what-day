3fe38547cc21be9f54b69acd1ea194e05f86c31a
Sentuar <thayward2001@gmail.com>
2023-11-22 09:38:36 +1300

    refactor: moved pexels create into mokced function

--- server/apiCalls/pexelApiCalls.ts
+++ server/apiCalls/pexelApiCalls.ts
@@ -2,9 +2,8 @@ import { createClient } from 'pexels'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const pexels = createClient(process.env.PEXELS_API_KEY || '')
-
 const getImages = async () => {
+  const pexels = createClient(process.env.PEXELS_API_KEY || '')
   const query = 'Nature'
   const images = await pexels.photos.search({ query, per_page: 1 })
   return images
