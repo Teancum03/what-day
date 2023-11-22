66711986fb34d1e5c272c4bae34d0c7c941c8952
Murray Sinclair <msin981@gmail.com>
2023-11-23 11:31:31 +1300

    feat: Friday- add SearchBar component

--- client/pages/Friday.tsx
+++ client/pages/Friday.tsx
@@ -2,13 +2,17 @@ import PageTitle from '@/components/PageTitle'
 import { useQuery } from '@tanstack/react-query'
 import { getImages } from '@/apis/galleryApi'
 import DisplayImage from '@/components/DisplayImage'
+import SearchBar from '@/components/SearchBar'
 
 function Friday() {
   const {
     data: images,
     isPending,
     error,
-  } = useQuery({ queryKey: ['gallery'], queryFn: getImages })
+  } = useQuery({ 
+    queryKey: ['gallery'], 
+    queryFn: () => getImages('default') 
+  })
 
   if (isPending) return <div>Loading...</div>
   if (error) return <div>Error:</div>
@@ -17,6 +21,7 @@ function Friday() {
     <>
       <PageTitle descriptor="Fun Frame" day="Friday" />
       <div className="text-xl tracking-tight md:text-2xl xl:text-3xl">
+        <SearchBar />
         {images.photos.map((image) => (
           <DisplayImage key={image.id} image={image} />
         ))}
