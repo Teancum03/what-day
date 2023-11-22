2dc40120b2276a6b28b588d7e4a02c51d8c5577d
Courtney Anderson <142571170+courtney-anderson@users.noreply.github.com>
2023-11-22 14:32:54 +1300

    test: addWins

--- server/db/functions/__tests__/winsDB.test.ts
+++ server/db/functions/__tests__/winsDB.test.ts
@@ -3,18 +3,30 @@ import { describe, it, expect, beforeAll, beforeEach } from 'vitest'
 
 import connection from '../../connection'
 
-beforeAll(async () => {
+beforeAll( async () => {
   await connection.migrate.latest()
 })
 
-beforeEach(async () => {
-  await connection.seed.run()
+beforeEach(async () =>  {
+   await connection.seed.run()
 })
 
 describe('getAllWins', () => {
   it('gets all the wins in the database', async () => {
     const allWins = await db.getAllWins()
     expect(allWins).toHaveLength(2)
-    expect(allWins[0].author).toBe('Anonymous Aardvark')
+    expect(allWins[0].author).toBe('Anonymous MiddleName Aardvark')
+  })
+})
+
+describe('addWins', () => {
+  it('adds a win to the database', async () => {
+    const currentWins = await db.getAllWins()
+    await db.addWin({
+      title: 'A Test Title',
+      author: 'A Test Author',
+    })
+    const allWins = await db.getAllWins()
+    expect(allWins).toHaveLength(currentWins.length+1)
   })
 })
