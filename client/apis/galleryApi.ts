3083793533b4d6d8f704672850851bbfb13621f4
Murray Sinclair <msin981@gmail.com>
2023-11-24 09:43:54 +1300

    refactor: galleryAPI remove unused imports and console.logs

--- client/apis/galleryApi.ts
+++ client/apis/galleryApi.ts
@@ -1,9 +1,7 @@
 import request from 'superagent'
-import { PexelsResponse, ImageSearch } from '@models/gallery'
+import { PexelsResponse } from '@models/gallery'
 
 export async function getImages(search: string): Promise<PexelsResponse> {
-  console.log(search);
-  
   const response = await request.get('/api/v1/gallery').query({ search })
   return response.body
 }
