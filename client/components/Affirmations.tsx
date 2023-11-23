bd5c6edfdfbd3286a1060f76d310fb5e3a7edc17
Teancum03 <144631723+Teancum03@users.noreply.github.com>
2023-11-23 20:29:17 +1300

    Update Affirmations.tsx

--- client/components/Affirmations.tsx
+++ client/components/Affirmations.tsx
@@ -25,7 +25,7 @@ export default function Affirmation() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold mb-4">Affirmation: {affirmation.affirmation}</h1>
+      <h1 className="text-2xl font-bold mb-4"> {affirmation.affirmation}</h1>
       <button
         onClick={handleGetNewAffirmation}
         className="bg-gray-400 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded text-sm"
